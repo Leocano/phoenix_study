6abfd7a1739d90189937c38d98a1d22798733917
Leonardo Cano <leocano4242@gmail.com>
2025-08-27 00:13:50 -0300

    Send flash message

--- lib/hello_phoenix_web/controllers/page_controller.ex
+++ lib/hello_phoenix_web/controllers/page_controller.ex
@@ -2,10 +2,12 @@ defmodule HelloPhoenixWeb.PageController do
   use HelloPhoenixWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    conn |> render(:home)
   end
 
   def redirect_test(conn, _params) do
-    conn |> redirect(to: ~p"/")
+    conn
+    |> put_flash(:error, "Let's pretend we have an error")
+    |> redirect(to: ~p"/")
   end
 end
